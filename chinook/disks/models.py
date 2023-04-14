6ce89f047a9595f90185fab9d48f9bbf5639080a
Guowei SUN <guowei.sun@student.isae-supaero.fr>
2023-04-14 10:07:02 +0200

    edited models and urls

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Track(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=50)
+    composer = models.CharField(max_length=100)
+    milliseconds = models.IntegerField
+    bytes = models.IntegerField
+    bytes = models.FloatField
+    unitPrice = models.IntegerField
+    albums = models.ForeignKey('Album',related_name="track_album")
+    
+class Album(models.Model):
+    id = models.IntegerField
+    title = models.CharField(max_length=100)
+    artists = models.ForeignKey('Artist',related_name="album_artist")
+    
+class Artist(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=50)
